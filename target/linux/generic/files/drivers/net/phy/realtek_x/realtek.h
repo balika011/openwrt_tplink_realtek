e04d01fdd486153373d83b0554e6402cc671a6d8
Balázs Triszka <info@balika011.hu>
2025-10-17 09:22:46 +0200

    initial RTL8251L / RTL8254B / RTL8261BE / RTL8261N / RTL8264 / RTL8264B
    Signed-off-by: Balázs Triszka <info@balika011.hu>

--- target/linux/generic/files/drivers/net/phy/realtek_x/realtek.h
+++ target/linux/generic/files/drivers/net/phy/realtek_x/realtek.h
@@ -5,6 +5,16 @@
 
 #include <linux/phy.h>
 
+#include "phy_patch.h"
+#include "phy_patch_rtl826x.h"
+
+struct rtl826x_priv {
+	rt_phy_patch_db_t *patch;
+	bool pnswap_tx : 1;
+	bool pnswap_rx : 1;
+	bool rtk_serdes_patch : 1;
+};
+
 int rtl822x_hwmon_init(struct phy_device *phydev);
 
 #endif /* REALTEK_H */
